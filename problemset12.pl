fd4ba330c0babfd45aca757e3da35709e4ad8ff2
Mennatullah Awadallah <52926455+PATH242@users.noreply.github.com>
2022-12-01 16:55:32 +0200

    Update problemset12.pl

--- problemset12.pl
+++ problemset12.pl
@@ -25,7 +25,7 @@ remove(X,[X|As], Rs):-  !, remove(X, As, Rs).
 remove(X,[A|As],[A|Rs]) :- remove(X,As,Rs).
 
 % 2.b
-removeU(X,[],[]).
+removeU(_X,[],[]).
 removeU(X, [A|As], Rs) :- X = A, !, removeU(X,As,Rs).
 removeU(X, [A|As], [A|Rs]) :- removeU(X, As, Rs).
 
@@ -33,13 +33,25 @@ removeU(X, [A|As], [A|Rs]) :- removeU(X, As, Rs).
 nat(0).
 nat(N) :- nat(K), N is K+1.
 
-nat(1, 1) :- !.
+nat(0, 0) :- !.
 nat(1, Max) :- Max > 1.
 nat(N, Max) :- M is Max-1, nat(K, M), N is K+1.
     
 % 3.a
 
-isPrime(N) :-  nat(X,N), nat(Y,N), (X < N), (Y < N) , (N is (X * Y)), !, fail.
-isPrime(N).
+isPrime(N) :-  nat(X,N), nat(Y,N), (N is (X * Y)), (X < N), (Y < N) ,  !, fail.
+isPrime(_N).
 
-prime(N) :- isPrime(N).
+prime(N) :- isPrime(N), nat(N), N > 1.
+
+% 3.b
+
+isCoprime(N,M) :-  
+    nat(X,N), nat(Y,M), 
+    Result1 is (N * Y), 
+    Result2 is (X * M),
+    Result1 = Result2,
+    (X < N), (Y < M) , !, fail.
+isCoprime(_N,_M).
+
+coprime(N,M) :- isCoprime(N,M), nat(N), N > 1, nat(M, N),  M > 1, N > M.
